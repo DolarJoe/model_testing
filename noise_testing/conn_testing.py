3b95e71670f2524a4016bac42c979ad14f747995
Janči <janrepkabb@gmail.com>
2025-10-22 14:49:27 +0200

    (づ ◕‿◕ )づ conn testing, now actually working

--- noise_testing/conn_testing.py
+++ noise_testing/conn_testing.py
@@ -1,8 +1,9 @@
 # change to the root directory of the project
 
+from re import S
 from tvb.simulator.models.oscillator import SupHopf
 from tvb.simulator.lab import simulator, connectivity, coupling
-from tvb.simulator.integrators import EulerStochastic
+from tvb.simulator.integrators import EulerStochastic, EulerDeterministic
 from tvb.simulator.monitors import Raw
 import tvb.simulator.lab as tvbl
 
@@ -14,16 +15,24 @@ class ModelWrapper:
 
     def __init__(self):
         self.dt = 0.1
-        self.noise = 0.0
-        self.size = 5
-        self.conn = np.zeros((self.size, self.size))
-        self.conn = connectivity.Connectivity.from_file()
+        self.make_conn()
         self.size = self.conn.weights.shape[0]
-        self.tract_len = np.zeros((self.size, self.size))
         self.a = 0.35
         self.w = 0.2
         self.sim_steps = 1
-        self.init_cond = np.r_[[[np.ones((self.size, 1)), np.ones((self.size, 1))]]]
+        np_rng = np.random.default_rng(seed=46)
+        self.init_cond = np.r_[[[np_rng.random((self.size, 1)), np_rng.random((self.size, 1))]]]
+
+    def make_conn(self):
+        # weights = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+        weights = np.zeros((3, 3))
+        self.conn = connectivity.Connectivity(weights=weights)
+        self.conn.compute_region_labels()
+        self.conn.centres_cubic()
+        self.conn = connectivity.Connectivity.from_file()
+        self.conn.tract_lengths = np.zeros_like(self.conn.weights)  # because of neurolib
+        np.fill_diagonal(self.conn.weights, 0)  # remove self-connections
+        self.conn.tract_lengths *= 0.0
 
     def run(self):
         pass
@@ -33,19 +42,10 @@ class TvbModel(ModelWrapper):
     def __init__(self):
         super().__init__()
 
-        conn = connectivity.Connectivity(
-            weights=self.conn,
-            tract_lengths=self.tract_len,
-        )
-
-        conn.centres_spherical(number_of_regions=self.size)
-        conn.create_region_labels()
-
         self.sim = simulator.Simulator(
-            connectivity=conn,
+            connectivity=self.conn,
             model=SupHopf(a=np.r_[self.a], omega=np.r_[self.w]),
-            integrator=EulerStochastic(dt=self.dt, noise=hiss),
-            # integrator=EulerDeterministic(dt=self.dt),
+            integrator=EulerDeterministic(dt=self.dt),
             initial_conditions=self.init_cond,
             monitors=[Raw()],
             simulation_length=self.dt * self.sim_steps,
@@ -61,17 +61,17 @@ class NeurolibModel(ModelWrapper):
     def __init__(self):
         super().__init__()
         self.model = HopfModel(
-            Cmat=self.conn,
-            Dmat=self.tract_len,
+            Cmat=self.conn.weights,
+            Dmat=self.conn.tract_lengths,
         )
-        self.model.params["xs_init"] = self.init_cond
-        self.model.params["ys_init"] = self.init_cond
+        self.model.params["dt"] = self.dt
+        self.model.params["xs_init"] = self.init_cond[0, 0]
+        self.model.params["ys_init"] = self.init_cond[0, 1]
         self.model.params["duration"] = self.dt * self.sim_steps
         self.model.params["a"] = self.a
         self.model.params["w"] = self.w
         self.model.params["coupling"] = "additive"
         self.model.params["K_gl"] = 1.0
-        self.model.params.sigma_ou = self.noise
         self.model.params.tau_ou = 1.0
 
     def run(self):
@@ -79,30 +79,22 @@ class NeurolibModel(ModelWrapper):
         return self.model.x
 
 
-# TODO init model 10 times then run it once
-def run_multi_nlb():
-    arr = np.array([])
-    for _ in range(10):
-        neurolibModel = NeurolibModel()
-        neurolib_result = neurolibModel.run()
-        np.append(arr, neurolib_result)
-    return arr
+def run_nlb():
+    neurolibModel = NeurolibModel()
+    return neurolibModel.run()
 
 
-def run_multi_tvb():
-    arr = np.array([])
-    for _ in range(10):
-        neurolibModel = NeurolibModel()
-        neurolib_result = neurolibModel.run()
-        np.append(arr, neurolib_result)
-    return arr
+def run_tvb(neurolib_result_shape):
+    tvbModel = TvbModel()
+    return np.reshape(tvbModel.run()[0][1].T, neurolib_result_shape)
 
 
 if __name__ == "__main__":
 
     tvbModel = TvbModel()
 
-    tvb_result = np.array([np.reshape(tvbModel.run()[0][1].T, neurolib_result[0].shape) for _ in range(10)])
+    neurolib_result = run_nlb()
+    tvb_result = run_tvb(neurolib_result.shape)
 
     print(f"nlb shape: {neurolib_result.shape}\ntvb shape: {tvb_result.shape}")
-    np.testing.assert_allclose(tvb_result, neurolib_result)
+    np.testing.assert_allclose(neurolib_result, tvb_result)
