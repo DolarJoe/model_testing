cac65f97875e538609bf9767c6e145b9fbafc070
Janči <janrepkabb@gmail.com>
2025-11-06 19:36:42 +0100

    ≽^•⩊•^≼ typo

--- connectivity_and_delay/config.py
+++ connectivity_and_delay/config.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tvb.simulator.lab as tvbl
 from tvb.simulator.models.oscillator import SupHopf
-from connectivity_and_delay.conn_no_warning import ConnNoWarnings
+from conn_no_warning import ConnNoWarnings
 
 
 class Config:
