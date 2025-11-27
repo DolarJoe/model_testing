32f9b70e16b1dd86fbf71ad1c9e9599b5ae21226
Janči Repka <jan.repka@ceitec.muni.cz>
2025-11-27 16:34:45 +0100

    ≽^•⩊•^≼ calculate history shape in a better way

--- connectivity_and_delay/config.py
+++ connectivity_and_delay/config.py
@@ -7,7 +7,6 @@ from conn_no_warning import ConnNoWarnings
 class Config:
     def __init__(self, initial_conditions_seed, noise_seed=42):
         self.dt = 0.1
-        self.sim_steps = 1
         self.speed = 2.0
         self.a = 0.35
         self.w = 0.2
@@ -28,9 +27,7 @@ class Config:
     def init_config_for_connectivity(self):
         self.__config_connectivity()
         self.conn.tract_lengths = np.zeros_like(self.conn.weights)  # because of neurolib
-        self.init_cond = np.r_[
-            [[self.init_cond_rng.random((self.size, 1)), self.init_cond_rng.random((self.size, 1))]]
-        ]
+        self.init_cond = np.r_[[[self.init_cond_rng.random((self.size, 1)), self.init_cond_rng.random((self.size, 1))]]]
 
     # TODO state variable count is hardcoded here, fix
     def init_config_for_delays(self):
@@ -38,9 +35,7 @@ class Config:
         max_len = np.max(self.conn.tract_lengths)
         self.conn.tract_lengths /= max_len
         self.conn.tract_lengths *= self.history_length - 1
-        # Maybe this is the right way to do it, I don't know
-        # init_hist_shape = int((np.max(self.conn.tract_lengths) / self.dt) / self.dt + 1)
-        # self.init_cond = self.np_rng.random((init_hist_shape, 2, self.size, 1))
+        self.conn.speed = np.r_[self.speed]
         self.init_cond = self.init_cond_rng.random(self.get_good_history_shape())
         self.conn.configure()
 
@@ -53,19 +48,12 @@ class Config:
         self.conn.try_compute_hemispheres()
         self.conn.configure()
         self.__config_connectivity()
-        self.init_cond = np.r_[[[np.zeros((self.size, 1)), np.zeros((self.size, 1))]]]
+        self.init_cond = np.r_[[[np.ones((self.size, 1)), np.ones((self.size, 1))]]]
 
-    # TODO model is hardcoded here, fix
     def get_good_history_shape(self):
         self.__config_connectivity()
-        # There is most assuredly a way to calculate this directly from the
-        # connectivity and dt, but I have made a mistake here so many times already
-        # that I will simply use TVB to do it for me.
-        temp_sim = tvbl.simulator.Simulator(
-            connectivity=self.conn,
-            model=SupHopf(),
-            integrator=tvbl.integrators.EulerDeterministic(dt=self.dt),
-            conduction_speed=self.speed,
-            simulation_length=self.dt * self.sim_steps,
-        ).configure()
-        return temp_sim.good_history_shape
+        delays = self.conn.tract_lengths / self.speed
+        idelays = np.rint(delays / self.dt).astype(np.int32)
+        init_hist_shape = np.max(idelays) + 1
+        init_cond_shape = (init_hist_shape, 2, self.size, 1)
+        return init_cond_shape
