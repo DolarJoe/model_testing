3f24b1782bc7af6f6e8a92a77817163468f22af0
Janči Repka <jan.repka@ceitec.muni.cz>
2025-12-15 18:27:00 +0100

    config handles number of variables

--- connectivity_and_delay/config.py
+++ connectivity_and_delay/config.py
@@ -15,28 +15,31 @@ class Config:
         self.coupling_strength = 1.0
         self.noise = 0.0
         self.noise_seed = noise_seed
+        self.number_of_state_variables = 2
         self.conn = None
 
-    def __config_connectivity(self):
+    def _config_connectivity(self):
         if self.conn is None:
             self.conn = ConnNoWarnings().from_file()
         np.fill_diagonal(self.conn.weights, 0)  # remove self-connections
         self.conn.speed = np.r_[self.speed]
         self.size = self.conn.weights.shape[0]
 
-    def init_config_for_connectivity(self):
-        self.__config_connectivity()
-        self.conn.tract_lengths = np.zeros_like(self.conn.weights)  # because of neurolib
-        self.init_cond = np.r_[[[self.init_cond_rng.random((self.size, 1)), self.init_cond_rng.random((self.size, 1))]]]
+    def init_config_for_dfun(self):
+        raise NotImplementedError("Dfun init not yet implemented")
 
-    # TODO state variable count is hardcoded here, fix
+    def init_config_for_connectivity(self):
+        self._config_connectivity()
+        self.conn.tract_lengths = np.zeros_like(self.conn.weights)  # because of neurolib
+        self.init_cond = self.init_cond_rng.random((1, self.number_of_state_variables, self.size, 1))
+
     def init_config_for_delays(self):
-        self.__config_connectivity()
+        self._config_connectivity()
         max_len = np.max(self.conn.tract_lengths)
         self.conn.tract_lengths /= max_len
         self.conn.tract_lengths *= self.history_length - 1
         self.conn.speed = np.r_[self.speed]
-        self.init_cond = self.init_cond_rng.random(self.get_good_history_shape())
+        self.init_cond = self.init_cond_rng.random(self._get_good_history_shape())
         self.conn.configure()
 
     def init_cond_for_noise(self):
@@ -47,13 +50,13 @@ class Config:
         self.conn.compute_region_labels()
         self.conn.try_compute_hemispheres()
         self.conn.configure()
-        self.__config_connectivity()
-        self.init_cond = np.r_[[[np.ones((self.size, 1)), np.ones((self.size, 1))]]]
+        self._config_connectivity()
+        self.init_cond = self.init_cond_rng.random((1, self.number_of_state_variables, self.size, 1))
 
-    def get_good_history_shape(self):
-        self.__config_connectivity()
+    def _get_good_history_shape(self):
+        self._config_connectivity()
         delays = self.conn.tract_lengths / self.speed
         idelays = np.rint(delays / self.dt).astype(np.int32)
         init_hist_shape = np.max(idelays) + 1
-        init_cond_shape = (init_hist_shape, 2, self.size, 1)
+        init_cond_shape = (init_hist_shape, self.number_of_state_variables, self.size, 1)
         return init_cond_shape
